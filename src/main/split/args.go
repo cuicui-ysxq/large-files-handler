e818a450f73531245f50bbc6d52602c77d145925
mobiledesres <45989029+mobiledesres@users.noreply.github.com>
2024-07-03 15:04:58 +0800

    feat: use `Args` interface

---
+++ src/main/split/args.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/cuicui-yxsq/large-files-handler/common"
+)
+
+type Args struct {
+	inputFilePath string
+	chunkSize     uint
+	outDir        string
+}
+
+func (a *Args) DefineArgs() {
+	flag.StringVar(&a.inputFilePath, "i", "", "input file path")
+	flag.UintVar(&a.chunkSize, "s", common.GitHubMaxRecommendedFileSize, "chunk size in bytes")
+	flag.StringVar(&a.outDir, "d", "", "output directory")
+}
+
+func (a *Args) Check() (errs []error) {
+	if a.inputFilePath == "" {
+		errs = append(errs, fmt.Errorf("missing input file path"))
+	}
+
+	return
+}
