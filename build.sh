32ab3fbeab5dff638abcbb0a43680d397c7584e7
mobiledesres <45989029+mobiledesres@users.noreply.github.com>
2024-07-03 14:59:36 +0800

    fix: bash find & build

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ BIN_DIR=$CURDIR/bin
 mkdir -p "$BIN_DIR"
 
 cd "$SRC_DIR"
-find . -name main.go -print0 | xargs -0 -I "{}" bash -c 'go build -o "'$BIN_DIR'/$(basename "$(dirname "{}")")" "{}"'
+find . -name main.go -print0 | xargs -0 -I "{}" bash -c 'DIR=$(dirname "{}"); EXEC_NAME=$(basename "$DIR"); go build -C "$DIR" -o "'"$BIN_DIR/$EXEC_NAME"'"'
